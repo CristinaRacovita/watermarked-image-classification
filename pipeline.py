f4438497970840a3c749ac106d7262b519a7145f
Bindila-Bogdan <b.mihaibogdan@yahoo.com>
2025-01-31 16:44:14 +0100

    rename or organize directories

--- pipeline.py
+++ pipeline.py
@@ -4,17 +4,17 @@ import os
 import json
 
 
-
 requirements_file = "requirements.txt"
 # Paths
-#notebooks_dir = "notebooks/"
+# notebooks_dir = "notebooks/"
 output_dir = "output/"
 output_der = "output/"
-äinput_model_name = "apple/mobilevit-xx-small" #default model is apple/mobilevit-xx-small
-input_model_name = "tiny_vit_5m_224" 
+äinput_model_name = "apple/mobilevit-xx-small"  # default model is apple/mobilevit-xx-small
+input_model_name = "tiny_vit_5m_224"
 
-#model_path = "apple_mobilevit-xx-small_2025-01-12_17-23.pth" #default model for testing pipline
+# model_path = "apple_mobilevit-xx-small_2025-01-12_17-23.pth" #default model for testing pipline
 model_path = f"models/tiny_vit_5m_224_2025-01-06_17-38.pth"
+
 
 # Function to install requirements
 def install_requirements():
@@ -25,8 +25,9 @@ def install_requirements():
         print(f"Error: {requirements_file} not found. Please ensure the file exists.")
         exit(1)
 
+
 # Install dependencies
-#install_requirements()
+# install_requirements()
 
 notebooks = {
     "watermarking": os.path.join("creating_watermarked_images.ipynb"),
@@ -34,8 +35,8 @@ notebooks = {
     "classify": os.path.join("classify_with_and_without_watermark.ipynb"),
     "removal": os.path.join("watermark_removal.ipynb"),
     "final_classify": os.path.join("classify_reconstruced_images.ipynb"),
-    "xai" :os.path.join("xai/pytorch_grad_cam_explanation.ipynb"),
-    "metrics" :os.path.join("xai/pytorch_grad_cam_explanation.ipynb")
+    "xai": os.path.join("pytorch_grad_cam_explanation.ipynb"),
+    "metrics": os.path.join("pytorch_grad_cam_explanation.ipynb"),
 }
 
 # Paths to save executed notebooks
@@ -45,23 +46,23 @@ executed_notebooks = {
     "classify": os.path.join("executed_classify.ipynb"),
     "removal": os.path.join(output_dir, "executed_removal.ipynb"),
     "final_classify": os.path.join(output_dir, "final_classify_notebook.ipynb"),
-     "xai" :os.path.join(output_dir, "executed_pytorch_grad_cam_explanation.ipynb"),
-    "metrics" :os.path.join(output_dir, "executed_metrics.ipynb")
+    "xai": os.path.join(output_dir, "executed_pytorch_grad_cam_explanation.ipynb"),
+    "metrics": os.path.join(output_dir, "executed_metrics.ipynb"),
 }
 
 # Run notebooks sequentially
 print("Starting pipeline...")
 
 print("Creating watermarked images...")
-#opacity parameters
-#pm.execute_notebook(
+# opacity parameters
+# pm.execute_notebook(
 #    notebooks["watermarking"],
 #    executed_notebooks["watermarking"],
 #    parameters={"output_dir": os.path.join(output_dir, "watermarked_images/")},
 #    kernel_name="python3"
-#)
+# )
 
-'''
+"""
 # Step 2: Train classifier and classify with/without watermark
 # Load one pretrained model
 #model_name = "apple/mobilevit-xx-small"
@@ -123,21 +124,21 @@ with open(metrics_path, "r") as f:
 # Print the metrics in the pipeline
 print(f"Prediction Metrics: {prediction_metrics}")
 
-''' 
+"""
 
 # Step 3: Remove watermarks
-#print("Removing watermarks...")
-#pm.execute_notebook(
+# print("Removing watermarks...")
+# pm.execute_notebook(
 #    notebooks["removal"],
 #    executed_notebooks["removal"],
 #    parameters={
 #        "watermarked_dir": os.path.join(output_dir, "watermarked_images/"),
 #        "cleaned_dir": os.path.join(output_dir, "cleaned_images/")
 #    }
-#)
+# )
 
-#show some pictures
-'''
+# show some pictures
+"""
 # Step 4: 
 #print("Classification of reconstructed images...")
 pm.execute_notebook(
@@ -158,7 +159,7 @@ with open(metrics_path, "r") as f:
 
 # Print the metrics in the pipeline
 print(f"Reconstructed Metrics: {reconstructed_metrics}")
-'''
+"""
 
 print("XAI evaluation...")
 pm.execute_notebook(
@@ -167,8 +168,8 @@ pm.execute_notebook(
     parameters={
         "output_dir": output_dir,
         "input_model_path": model_path,
-        "input_model_name": input_model_name
-    }
+        "input_model_name": input_model_name,
+    },
 )
 
 # Path to XAI outputs
@@ -179,9 +180,10 @@ print(f"Grad-CAM output available at: {xai_output_path}")
 # Optionally, display the image in an interactive session
 # This requires an environment like Jupyter Notebook or JupyterLab
 from IPython.display import Image, display
+
 display(Image(filename=xai_output_path))
 
-#show outputs etc.
+# show outputs etc.
 
 print("Metrics evaluation...")
 pm.execute_notebook(
@@ -190,8 +192,8 @@ pm.execute_notebook(
     parameters={
         "output_dir": output_dir,
         "input_model_path": model_path,
-        "input_model_name": input_model_name
-    }
+        "input_model_name": input_model_name,
+    },
 )
 
 # Path to metrics outputs
@@ -202,6 +204,7 @@ print(f"Metric output available at: {metrics_output_path}")
 # Optionally, display the image in an interactive session
 # This requires an environment like Jupyter Notebook or JupyterLab
 from IPython.display import Image, display
+
 display(Image(filename=metrics_output_path))
 
 
